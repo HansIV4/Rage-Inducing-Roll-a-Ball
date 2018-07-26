e777e83a7ed952b5532458019ab7a6d3c23fbc00
Unknown <HANSIV4@OUTLOOK.COM>
2018-07-26 18:50:02 -0400

    First push

---
+++ BallGame/Assets/scripts/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float speed;
+    public Text countText, winText;
+    private int count;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+        winText.text ="";
+    }
+
+    void FixedUpdate()
+    {
+       
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal,Math.Abs(moveHorizontal + moveVertical * 2), moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("pickup"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    private void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        if (count == 7)
+            winText.text = "You Win FGT";
+    }
+
+}
+
