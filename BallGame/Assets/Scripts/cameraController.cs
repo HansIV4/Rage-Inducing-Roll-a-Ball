4f14321618f05dfa57699c76c5438824165a210b
hansiv4 <hansiv4@outlook.com>
2018-07-27 17:18:26 -0400

    Added end game menu

---
+++ BallGame/Assets/Scripts/cameraController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraController : MonoBehaviour {
+
+    public GameObject player;
+    private Vector3 offset;
+     
+	// Use this for initialization
+	void Start () {
+        offset = transform.position - player.transform.position;	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        transform.position = player.transform.position + offset;
+        if (player.transform.position.y < -10)
+        {
+            player.transform.position =new Vector3(0,5,0);
+            //transform.position = startPosition;
+            
+        }
+	}
+}
